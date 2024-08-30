7d58c44230bdce0458c5541635df9aa511f07491
sergiojulio <sergiojulio@gmail.com>
2024-08-30 14:38:59 -0400

    code changes

--- main.py
+++ main.py
@@ -11,115 +11,11 @@ load_dotenv(dotenv_path=dotenv_path)
 
 # mastodon access token
 api_key = os.getenv('API_KEY')
+#enviroment = os.getenv('ENVIROMENT') # dev - prd (butquet)
 
 
-def test(date='2024-08-02'):
-    date_from = date + ' 00:00:00'
-    date_to = date + ' 23:59:59'
 
-    dt = datetime.strptime(date_from, "%Y-%m-%d %H:%M:%S")
-
-    print(int(dt.replace(tzinfo=timezone.utc).timestamp()))
-
-    print(int(datetime.strptime(date_from, "%Y-%m-%d %H:%M:%S").timestamp()))
-    print(int(datetime.strptime(date_to, "%Y-%m-%d %H:%M:%S").timestamp()))   
-
-
-#EXTRACT
-def extract(date='2024-08-03'):
-    try:
-
-        # date to timestamp
-        date_from = date + ' 00:00:00'
-        date_to = date + ' 23:59:59'
-
-        dt = datetime.strptime(date_from, "%Y-%m-%d %H:%M:%S")
-        date_from = int(dt.replace(tzinfo=timezone.utc).timestamp())
-
-        dt = datetime.strptime(date_to, "%Y-%m-%d %H:%M:%S")
-        date_to = int(dt.replace(tzinfo=timezone.utc).timestamp())
-
-
-        response = requests.get('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=radioheadve&api_key=' + api_key + '&from=' + str(date_from) + '&to=' + str(date_to) + '&format=json')
-        #print(response.text)
-
-        response.raise_for_status()
-        # access JSOn content
-        json_response = response.json()
-
-        # init list headers
-
-        pages = int(json_response['recenttracks']['@attr']['totalPages'])
-        i = 0
-        c = pages
-
-        lists = 'timestamp,date_text,artist,artist_mbid,album,album_mbid,track,track_mbid\n'
-
-        while i < pages:
-            print(i)
-            i += 1
-            # return list
-            list = get_tracks(start=date_from, end=date_to, page=c)
-            c -= 1
-            lists = lists + list
-            
-        # close file
-        with open('./csv/tracks-' + date + '.csv', 'w') as f:
-            f.write(lists)
-            f.close()
-
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
-    except Exception as err:
-        print(f'Other error occurred 1: {err}')
-
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(exc_type, fname, exc_tb.tb_lineno)
-    
-
-def get_tracks(start, end, page):
-    
-    try:
-        # request
-        response = requests.get('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=radioheadve&api_key=' + api_key + '&from=' + str(start) + '&to=' +  str(end) + '&page=' + str(page) + '&format=json')
-    
-        # print('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=radioheadve&api_key=' + api_key + '&from=' + str(start) + '&to=' +  str(end) + '&page=' + str(page) + '&format=json')
-
-        lists = ''
-
-        json_response = response.json()
-        # invert list
-        tracks = json_response['recenttracks']['track']
-        for track in reversed(tracks):
-
-            if "date" in track:
-
-                list = ('"' + track['date']['uts'] + '",' +
-                        '"' + track['date']['#text'] + '",' +
-                        '"' + track['artist']['#text'] + '",' +
-                        '"' + track['artist']['mbid'] + '",' +
-                        '"' + track['album']['#text'] + '",' +
-                        '"' + track['album']['mbid'] + '",' +
-                        '"' + track['name'] + '",' +
-                        '"' + track['mbid'] + '"\n')     
-                
-                lists = lists + list 
-
-        return lists
-    
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
-    except Exception as err:
-        print(f'Other error occurred 2: {err}')
-
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(exc_type, fname, exc_tb.tb_lineno)
-    
-    return True
-
-# Create de database and schema
+# Create de database and schematas
 def warehouse():
 
     from pyiceberg.schema import Schema
@@ -130,13 +26,18 @@ def warehouse():
 
     warehouse_path = "./warehouse"
 
+    # data warehouse
+
     catalog = SqlCatalog(
-        "default",
+        "lastfm",
         **{
             "uri": f"sqlite:///{warehouse_path}/pyiceberg_catalog.db",
             "warehouse": f"file://{warehouse_path}",
         },
     )
+
+
+     # processing   
 
     schema = Schema(
         NestedField(field_id=1, name='timestamp', field_type=LongType(), required=False),
@@ -150,12 +51,32 @@ def warehouse():
         NestedField(field_id=9, name='date', field_type=DateType(), required=False)
     )
 
-    catalog.create_namespace("stage")
+    catalog.create_namespace("silver")
 
     partition_spec = PartitionSpec(PartitionField(source_id=9, field_id=9, transform=IdentityTransform(), name="date"), spec_id=1)
 
     catalog.create_table(
-        "stage.tracks",
+        "silver.tracks",
+        schema=schema,
+        partition_spec=partition_spec
+    ) 
+
+    # consumption
+    
+    schema = Schema(
+        NestedField(field_id=1, name='datetime', field_type=TimestampType(), required=False),
+        NestedField(field_id=2, name='artist', field_type=StringType(), required=False),
+        NestedField(field_id=3, name='album', field_type=StringType(), required=False),
+        NestedField(field_id=4, name='track', field_type=StringType(), required=False),
+        NestedField(field_id=5, name='date', field_type=DateType(), required=False)
+    )
+
+    catalog.create_namespace("gold")
+
+    partition_spec = PartitionSpec(PartitionField(source_id=5, field_id=5, transform=IdentityTransform(), name="date"), spec_id=1)
+
+    catalog.create_table(
+        "gold.tracks",
         schema=schema,
         partition_spec=partition_spec
     ) 
@@ -167,37 +88,36 @@ def drop_table():
     warehouse_path = "./warehouse"
 
     catalog = SqlCatalog(
-        "default",
+        "lasftfm",
         **{
             "uri": f"sqlite:///{warehouse_path}/pyiceberg_catalog.db",
             "warehouse": f"file://{warehouse_path}",
         },
     )  
 
-    catalog.drop_table("default.tracks")
+    catalog.drop_table("silver.tracks")
+    catalog.drop_table("gold.tracks")
 
 
-def query():
+def query(schema):
 
     from pyiceberg.catalog.sql import SqlCatalog
 
     warehouse_path = "./warehouse"
 
     catalog = SqlCatalog(
-        "default",
+        "lastfm",
         **{
             "uri": f"sqlite:///{warehouse_path}/pyiceberg_catalog.db",
             "warehouse": f"file://{warehouse_path}",
         },
     )  
 
-
-    table = catalog.load_table(('default', 'tracks'))
+    table = catalog.load_table((schema, 'tracks'))
 
     #print(table.describe())
 
     con = table.scan().to_duckdb(table_name="tracks")
-
 
     con.sql(
         "SELECT date, COUNT(*) total FROM tracks GROUP BY date"
@@ -243,7 +163,7 @@ def create_table():
 
     #1722556800
     #999999999
-    df = pc.read_csv("./csv/tracks-2024-08-02.csv")
+    df = pc.read_csv(warehouse_path + "raw/tracks-2024-08-02.csv")
 
     #df = df.append_column("date", pa.array([1722571] * len(df), pa.date32()))
     df = df.append_column("date", pa.array([date(2024, 8, 2)] * len(df), pa.date32()))
@@ -282,37 +202,123 @@ def create_table():
     table.overwrite(df)
 
 
+#EXTRACT
+def extract(date):
+    try:
+
+        # date to timestamp
+        date_from = date + ' 00:00:00'
+        date_to = date + ' 23:59:59'
+
+        dt = datetime.strptime(date_from, "%Y-%m-%d %H:%M:%S")
+        date_from = int(dt.replace(tzinfo=timezone.utc).timestamp())
+
+        dt = datetime.strptime(date_to, "%Y-%m-%d %H:%M:%S")
+        date_to = int(dt.replace(tzinfo=timezone.utc).timestamp())
+
+        response = requests.get('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=radioheadve&api_key=' + api_key + '&from=' + str(date_from) + '&to=' + str(date_to) + '&format=json')
+
+        response.raise_for_status()
+        json_response = response.json()
+
+        pages = int(json_response['recenttracks']['@attr']['totalPages'])
+        i = 0
+        c = pages
+
+        lists = 'timestamp,date_text,artist,artist_mbid,album,album_mbid,track,track_mbid\n'
+
+        while i < pages:
+            i += 1
+            # return list
+            list = get_tracks(start=date_from, end=date_to, page=c)
+            c -= 1
+            lists = lists + list
+            
+        with open('./warehouse/raw/tracks-' + date + '.csv', 'w') as f:
+            f.write(lists)
+            f.close()
+
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred 1: {err}')
+        # error details
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+    
+
+def get_tracks(start, end, page):
+    
+    try:
+        # request
+        response = requests.get('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=radioheadve&api_key=' + api_key + '&from=' + str(start) + '&to=' +  str(end) + '&page=' + str(page) + '&format=json')
+    
+        lists = ''
+
+        json_response = response.json()
+        # invert list
+        tracks = json_response['recenttracks']['track']
+        for track in reversed(tracks):
+
+            if "date" in track:
+
+                list = ('"' + track['date']['uts'] + '",' +
+                        '"' + track['date']['#text'] + '",' +
+                        '"' + track['artist']['#text'] + '",' +
+                        '"' + track['artist']['mbid'] + '",' +
+                        '"' + track['album']['#text'] + '",' +
+                        '"' + track['album']['mbid'] + '",' +
+                        '"' + track['name'] + '",' +
+                        '"' + track['mbid'] + '"\n')     
+                
+                lists = lists + list 
+
+        return lists
+    
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred 2: {err}')
+
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+    
+    return True
+
+
 #LOAD
-def write_data(date='2024-08-03'):
+def load(date):
 
-    from datetime import date
+    from datetime import date as dt
     from pyiceberg.catalog.sql import SqlCatalog
-
-    # pandas
     import pyarrow as pa
     import pyarrow.csv as pc
 
     warehouse_path = "./warehouse"
 
     catalog = SqlCatalog(
-        "default",
+        "lastfm",
         **{
             "uri": f"sqlite:///{warehouse_path}/pyiceberg_catalog.db",
             "warehouse": f"file://{warehouse_path}",
         },
     )
 
-    df = pc.read_csv("./csv/tracks-2024-08-03.csv")
+    table = catalog.load_table("silver.tracks")
 
-    df = df.append_column("date", pa.array([date(2024, 8, 3)] * len(df), pa.date32()))
+    df = pc.read_csv("./csv/tracks-" + date + ".csv")
 
-    table = catalog.load_table("default.tracks")
+    year, month, day = map(int, date.split('-'))
 
-    table.overwrite(df)    
+    df = df.append_column("date", pa.array([dt(year, month, day)] * len(df), pa.date32()))
+
+    table.append(df)    
 
 
 #TRANSFORM 
-def transformation(date="2024-08-03"):
+def transformation(date):
 
     from pyiceberg.catalog.sql import SqlCatalog
     from pyiceberg.expressions import EqualTo
@@ -323,48 +329,45 @@ def transformation(date="2024-08-03"):
     warehouse_path = "./warehouse"
 
     catalog = SqlCatalog(
-        "default",
+        "lastfm",
         **{
             "uri": f"sqlite:///{warehouse_path}/pyiceberg_catalog.db",
             "warehouse": f"file://{warehouse_path}",
         },
     )  
 
-    table = catalog.load_table(('default', 'tracks'))
+    silver_table = catalog.load_table(('silver', 'tracks'))
 
-    df = table.scan(
+    silver_df = silver_table.scan(
         row_filter = EqualTo("date", date),
-        selected_fields = ("timestamp","date_text","artist","artist_mbid","album","album_mbid","track","track_mbid","date"),
+        selected_fields = ("timestamp","artist","album","track","date"),
     ).to_arrow()
 
-    #pc.strptime(df.column("simestamp"), format='%Y-%m-%d %H:%M:%S', unit='s')
+    col = pc.cast(silver_df["timestamp"], pa.timestamp('s'))
+
+    silver_df = silver_df.drop("timestamp")
+
+    silver_df = silver_df.append_column("datetime", col)
+
+    # gold
+
+    gold_table = catalog.load_table(('gold', 'tracks'))
+
+    with gold_table.update_schema() as update_schema:
+        update_schema.union_by_name(silver_df.schema)    
 
 
-    #df = df.append_column("date", pa.array([date(2024, 8, 3)] * len(df), pa.date32()))
+    gold_table.append(silver_df) 
 
-    col = pc.cast(df["timestamp"], pa.timestamp('s'))
-
-    df = df.append_column("datetime", col)
-    # 
-
-    print(df)
-
-    #table.append(df) 
-
-def temp():
-
-    import pyarrow as pa
-    import pyarrow.compute as pc
-
-    # Assuming '1722643317' is an epoch time (in seconds)
-    array = pa.array([1722643317], pa.int64())
-
-    # Cast the integer array to a timestamp
-    timestamp_array = pc.cast(array, pa.timestamp('s'))
-
-    print(timestamp_array)
 
 if __name__ == '__main__':
-    transformation()
+
+    #warehouse()
+    date = "2024-08-02"
+    extract(date)
+    load(date)
+    query('silver')
+    transformation(date)
+    query('gold')
 
 
